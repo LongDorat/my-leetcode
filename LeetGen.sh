bdd62befb40ff464fddac77cd90fe1a6211dca49
LongDorat <longdorat@outlook.com>
2026-05-08 17:01:58 +0000

    feat(LeetGen): add debug output and template directories to script Create directories for debug output and templates in the LeetGen script to enhance organization and facilitate debugging processes.

--- LeetGen.sh
+++ LeetGen.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-projectDir=./LeetGen/LeetGen/
-outputDir=./Problems/
-templateDir=./Templates/
+currentDir=$(pwd)
+projectDir=$currentDir/LeetGen/LeetGen/
+outputDir=$currentDir/Problems/
+outputDirDebug=$currentDir/.debug-output/
+templateDir=$currentDir/Templates/
+templateDirDebug=$currentDir/.debug-templates/
 
 dotnet restore $projectDir
 
 if [ $1 == "debug" ]; then
+    mkdir -p $outputDirDebug
+    mkdir -p $templateDirDebug
     dotnet build $projectDir -c Debug
     isDebug=true
 else
     dotnet build $projectDir -c Release
 fi
 
+echo "================================"
 read -p "Build complete. Do you want create or remove a project? (create/remove) " action
 read -p "What is the problem number? " problemNumber
 read -p "What is the programming language? " language
 
 if [ $action == "create" ]; then
     if [ $isDebug ]; then
-        dotnet run --project $projectDir -- create --output $outputDir --template $templateDir --problem $problemNumber --language $language --debug
+        dotnet run --project $projectDir -c Debug -- create --output $outputDirDebug --template $templateDirDebug --problem $problemNumber --language $language --debug
     else
-        dotnet run --project $projectDir -- create --output $outputDir --template $templateDir --problem $problemNumber --language $language
+        dotnet run --project $projectDir -c Release -- create --output $outputDir --template $templateDir --problem $problemNumber --language $language
     fi
 fi
