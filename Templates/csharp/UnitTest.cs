6f596291f870532487c320604c5b339e146ff5ca
LongDorat <longdorat@outlook.com>
2026-05-25 02:14:29 +0000

    feat(templates): add C# solution and unit test template

---
+++ Templates/csharp/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿namespace Solution;
+
+public class UnitTest
+{
+    private readonly Solution _solution = new Solution();
+    public UnitTest(){}
+
+    [Fact]
+    public void Test()
+    {
+        var result = _solution.Run();
+        // Add assertions here
+    }
+}
